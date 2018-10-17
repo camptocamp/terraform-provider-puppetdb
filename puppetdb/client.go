b139c4de40eda9bd97c0cf07cab14c599d8702b3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-10-17 14:45:35 +0200

    Accept SSL parameters as strings

--- puppetdb/client.go
+++ puppetdb/client.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -41,6 +42,10 @@ type commandsPayload struct {
 	Payload map[string]string `json:"payload"`
 }
 
+func isFile(str string) bool {
+	return strings.HasPrefix(str, "/")
+}
+
 func (p *PuppetDBClient) Query(query string, verb string, payload string) (pdbResp PuppetDBResp, err error) {
 	url := p.URL + "/pdb/" + query
 	form := strings.NewReader(payload)
@@ -54,15 +59,39 @@ func (p *PuppetDBClient) Query(query string, verb string, payload string) (pdbRe
 	var client *http.Client
 	if p.Cert != "" {
 		// Load cert pair
-		cert, err := tls.LoadX509KeyPair(p.Cert, p.Key)
-		if err != nil {
-			return pdbResp, err
+		var cert tls.Certificate
+		if isFile(p.Cert) {
+			if !isFile(p.Key) {
+				err = fmt.Errorf("cert points to a file but key is a string: %s", err)
+				return
+			}
+
+			cert, err = tls.LoadX509KeyPair(p.Cert, p.Key)
+			if err != nil {
+				return pdbResp, err
+			}
+		} else {
+			if isFile(p.Key) {
+				err = fmt.Errorf("cert is a string but key points to a file: %s", err)
+				return
+			}
+
+			cert, err = tls.X509KeyPair([]byte(p.Cert), []byte(p.Key))
+			if err != nil {
+				err = fmt.Errorf("failed to load client cert from string: %s", err)
+				return
+			}
 		}
 
 		// Load CA cert
-		caCert, err := ioutil.ReadFile(p.CA)
-		if err != nil {
-			return pdbResp, err
+		var caCert []byte
+		if isFile(p.CA) {
+			caCert, err = ioutil.ReadFile(p.CA)
+			if err != nil {
+				return pdbResp, fmt.Errorf("failed to load CA cert at %s: %s", p.CA, err)
+			}
+		} else {
+			caCert = []byte(p.CA)
 		}
 		caCertPool := x509.NewCertPool()
 		caCertPool.AppendCertsFromPEM(caCert)
